624a5fc7f6b04ad17dbc599713853b4e9d0d2125
Tom Lamb <lambtsa.tom@gmail.com>
2023-01-30 11:07:11 +0100

    Add test env and refactor knexfile

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -4,21 +4,16 @@ declare namespace NodeJS {
    */
   interface ProcessEnv {
     readonly PORT: string;
+
     readonly SPOTIFY_CLIENT_ID: string;
     readonly SPOTIFY_CLIENT_SECRET: string;
-
     readonly YOUTUBE_API_KEY: string;
 
-    readonly AWS_DB_PASSWORD: string;
-    readonly AWS_DB_HOST: string;
-    readonly AWS_DB_USER: string;
-    readonly AWS_DB_PORT: number;
-    readonly AWS_DATABASE: string;
-    readonly AWS_DB_PASSWORD_DEV: string;
-    readonly AWS_DB_HOST_DEV: string;
-    readonly AWS_DB_USER_DEV: string;
-    readonly AWS_DB_PORT_DEV: number;
-    readonly AWS_DATABASE_DEV: string;
+    readonly POSTGRES_PASSWORD: string;
+    readonly POSTGRES_HOST: string;
+    readonly POSTGRES_USER: string;
+    readonly POSTGRES_PORT: number;
+    readonly POSTGRES_DB: string;
   }
 }
 
